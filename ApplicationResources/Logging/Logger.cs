a6344a8f98e71a77a1a8db9d5656ef035de526a1
Matthew Slesinski <mslesinski12@gmail.com>
2022-06-15 23:18:36 -0400

    Update Logger.cs

--- ApplicationResources/Logging/Logger.cs
+++ ApplicationResources/Logging/Logger.cs
@@ -80,7 +80,8 @@ namespace ApplicationResources.Logging
             {
                 if (Settings.TryGet<string>(BasicSettings.LoggerConfigurationFile, out var configFileLocation))
                 {
-                    var filepath = configFileLocation;
+                    var logDirectoryParent = Settings.Get<string>(BasicSettings.ProjectRootDirectory);
+                    var filepath = Path.Combine(logDirectoryParent, configFileLocation);
                     var dataStoreAccessor = GlobalDependencies.Get<IDataStoreAccessor>();
                     if (await dataStoreAccessor.ExistsAsync(filepath, cancellationToken).WithoutContextCapture())
                     {
